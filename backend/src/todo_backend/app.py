513074d754b734a0df4ab404c173aaab59e557a6
Nathanael Thomas <nathanaelpthomas@gmail.com>
2025-11-03 13:59:05 +0100

    chore(backend): add .env and basic config loading

--- backend/src/todo_backend/app.py
+++ backend/src/todo_backend/app.py
@@ -1,4 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+load_dotenv()
+os.getenv("DATABASE_URL", "sqlite:///./dev.db")
+
+print("Loading .env file...")
+print("PP_ENV", os.getenv("PP_ENV"))
+print("PORT", os.getenv("PORT"))
+print("DATABASE_URL", os.getenv("DATABASE_URL"))
 
 app = FastAPI()
 
