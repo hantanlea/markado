6fa85d460e6a53244c52aad933d07bd0f7e72e79
Nathanael Thomas <nathanaelpthomas@gmail.com>
2025-11-06 11:16:29 +0100

    chore: fix broken env.py

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -6,7 +6,11 @@ from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
+from todo_backend import models  # noqa: F401
+
 load_dotenv()
+
+print(models.Task)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
